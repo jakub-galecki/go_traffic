b71dacc235420500620da8757fe1e9ded16b8f6d
jakub-galecki <galecki792@gmail.com>
2025-04-14 00:00:22 +0200

    cli

--- main.go
+++ main.go
@@ -2,10 +2,16 @@ package main
 
 import (
 	"fmt"
-	"github.com/urfave/cli/v2"
 	"log"
+	"log/slog"
 	"os"
+
+	"github.com/urfave/cli/v2"
 )
+
+func init() {
+	_ = slog.SetLogLoggerLevel(slog.LevelDebug)
+}
 
 type handler interface {
 	do(*req) error
@@ -25,7 +31,7 @@ func (g *trafficGenerator) run(c *cli.Context) error {
 		return fmt.Errorf("handler for traffic type not found: %s", c.String("type"))
 	}
 
-	r := newReq(c)
+	r := newReq(c, t)
 	return g.handlers[t].do(r)
 }
 
@@ -41,10 +47,33 @@ func main() {
 		Usage: "Generate http traffic for stress testing applications",
 		Flags: []cli.Flag{
 			&cli.StringFlag{
+				Name:     "url",
+				Required: true,
+				Usage:    "url of the target to be tested",
+				Aliases:  []string{"u"},
+			},
+			&cli.StringFlag{
 				Name:    "type",
 				Value:   "http",
 				Usage:   "type of traffic that should be generated: http",
 				Aliases: []string{"t"},
+			},
+			&cli.StringFlag{
+				Name:  "timeout",
+				Value: "500ms",
+				Usage: "for how long traffic should be generated",
+			},
+			&cli.Int64Flag{
+				Name:    "size",
+				Value:   10,
+				Usage:   "how many requests should be generated",
+				Aliases: []string{"s"},
+			},
+			&cli.Int64Flag{
+				Name:    "workers",
+				Value:   10,
+				Usage:   "number of concurrent workers that will generate request traffic",
+				Aliases: []string{"w"},
 			},
 		},
 		Action: gen.run,
--- request.go
+++ request.go
@@ -1,6 +1,10 @@
 package main
 
-import "github.com/urfave/cli/v2"
+import (
+	"github.com/urfave/cli/v2"
+	"log/slog"
+	"time"
+)
 
 type trafficType int
 
@@ -18,8 +22,29 @@ func trafficTypeFromString(s string) trafficType {
 
 type req struct {
 	ctx *cli.Context
+
+	parsed struct {
+		target  string
+		tt      trafficType
+		timeout time.Duration
+		count   int
+		workers int
+	}
 }
 
-func newReq(ctx *cli.Context) *req {
-	return &req{ctx: ctx}
+func newReq(ctx *cli.Context, t trafficType) *req {
+	r := &req{ctx: ctx}
+	r.parsed.target = ctx.String("url")
+	r.parsed.timeout = ctx.Duration("timeout")
+	r.parsed.count = ctx.Int("size")
+	r.parsed.workers = ctx.Int("workers")
+	r.parsed.tt = t
+
+	slog.Debug("[newReq] created request",
+		slog.String("url", r.parsed.target),
+		slog.Duration("timeout", r.parsed.timeout),
+		slog.Int("count", r.parsed.count),
+		slog.Int("workers", r.parsed.workers))
+
+	return r
 }
